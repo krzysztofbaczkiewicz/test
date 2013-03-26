6841a0a54a17cb058f843b571b720c80e6623462
Krzysztof Bączkiewicz <krzysztofbaczkiewicz@outlook.com>
2013-03-26 12:42:27 +0100

    Test

---
+++ index.php
@@ -0,0 +1,17 @@
+<meta charset="UTF-8" />
+<pre>
+<?php
+	
+	function moja_funkcja($a, $b) {
+		
+		return $a + $b;		
+		
+	}
+	
+	$a = 1;
+	$b = 7;
+	
+	echo "Suma $a + $b = " . moja_funkcja($a, $b);
+
+?>
+</pre>
